f057ba2478859ef31df188bd076bff3b2fb35a07
Leonardy Kristianto <alendory@gmail.com>
2015-10-19 18:29:53 +0800

    SSV v0.1

---
+++ client/views/private.js
@@ -0,0 +1,33 @@
+Template.private.Members = function() {
+	return Members;
+}
+
+Template.private.helpers({
+	tableSettings: function() {
+		return {
+			collection: Members,
+			rowsPerPage: 20,
+			showFilter: true,
+			fields: [
+				{ key: '_id', label: 'Student ID' },
+				{ key: 'name', label: 'Student Name' },
+				{ key: 'nric_number', label: 'NRIC' },
+				{ key: 'passport_number', label: 'Passport No.' },
+				{ key: 'course.0.name', label: 'Course' },
+				{ key: 'contact_number', label: 'Contact No.' },
+				{ key: 'emergency_contact.0.name', label: 'Emergency Contact', hidden: true },
+				{ key: 'emergency_contact.0.number', label: 'Emergency Contact No.', hidden: true },
+				{
+					key: 'renewed_at',
+					label: 'Last Renewed',
+					fn: function(value) {
+						return moment(value).fromNow();
+					}
+				},
+			],
+			showRowCount: true,
+			showColumnToggles: true,
+			class: 'table table-striped table-hover col-sm-12',
+		};
+	}
+});
